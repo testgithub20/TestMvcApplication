95efbbd5a6ccac3d071ecc498c73638d7cfad1ef
testgithub20 <testgithub20@gmail.com>
2015-11-11 07:30:05 +0200

    First Test Commit

---
+++ TestMvcApplication/Controllers/JobsController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Web.Mvc;
+using TestMvcApplication.Models;
+
+namespace TestMvcApplication.Controllers
+{
+    public class JobsController : Controller
+    {
+        //
+        // GET: /Jobs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }*/
+
+        // 
+        // GET: /Jobs/Welcome/ 
+        
+        public ActionResult Welcome()
+        {
+            var json = "";
+            var url = "https://gist.githubusercontent.com/WillemLabu/34cfb50187ec334c48ee/raw/cb46400505afd82d9e354b591ad71d97f07613be/jobs.json";
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(url);
+            }
+
+            string jsonFixed = json.Replace("-", "");
+            var jobsList = JsonConvert.DeserializeObject<RootObject>(jsonFixed);
+            
+            return View(jobsList);
+        }
+    }
+}
---
+++ TestMvcApplication/Models/Jobs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace TestMvcApplication.Models
+{    
+    public class Jobs
+    {
+        public string client { get; set; }
+        public int jobnumber { get; set; }
+        public string jobname { get; set; }
+        public string due { get; set; }
+        public string status { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Jobs> jobs { get; set; }
+    }
+
+    public class JobsDBContext : DbContext
+    {
+        public DbSet<Jobs> Jobs { get; set; }
+    }
+}
